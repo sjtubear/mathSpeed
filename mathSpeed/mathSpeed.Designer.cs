5ec99f4d3d44b13fe61eadfddc11c2e89ae2cfc4
sjtubear <wangth25@aliyun.com>
2014-12-18 20:22:48 +0800

    Form fixed

---
+++ mathSpeed/mathSpeed.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace mathSpeed
+{
+    partial class mathSpeed
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbAnswer = new System.Windows.Forms.TextBox();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.butStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tiCount = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tbAnswer
+            // 
+            this.tbAnswer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbAnswer.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tbAnswer.Location = new System.Drawing.Point(310, 78);
+            this.tbAnswer.Name = "tbAnswer";
+            this.tbAnswer.Size = new System.Drawing.Size(118, 35);
+            this.tbAnswer.TabIndex = 0;
+            this.tbAnswer.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbAnswer_KeyDown);
+            // 
+            // lblTest
+            // 
+            this.lblTest.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTest.AutoEllipsis = true;
+            this.lblTest.AutoSize = true;
+            this.lblTest.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblTest.Location = new System.Drawing.Point(81, 77);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(193, 29);
+            this.lblTest.TabIndex = 1;
+            this.lblTest.Text = "Next Problem";
+            // 
+            // butStart
+            // 
+            this.butStart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.butStart.Enabled = false;
+            this.butStart.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butStart.Location = new System.Drawing.Point(145, 204);
+            this.butStart.Name = "butStart";
+            this.butStart.Size = new System.Drawing.Size(165, 91);
+            this.butStart.TabIndex = 2;
+            this.butStart.Text = "START";
+            this.butStart.UseVisualStyleBackColor = true;
+            this.butStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana Ref", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(388, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SCORE";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // mathSpeed
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 355);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.butStart);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.tbAnswer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "mathSpeed";
+            this.Text = "mathSpeed";
+            this.Load += new System.EventHandler(this.mathSpeed_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbAnswer;
+        private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.Button butStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer tiCount;
+    }
+}
+
---
+++ mathSpeed/mathSpeed.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mathSpeed
+{
+    public partial class mathSpeed : Form
+    {
+        public mathSpeed()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mathSpeed_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbAnswer_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("entered!");
+        }
+
+        private void tbAnswer_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                MessageBox.Show("Enter!");
+            }
+        }
+    }
+}
